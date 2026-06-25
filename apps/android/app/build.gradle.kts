cc25b56ec4ebd74c2b9aa2772950b00a476d9c81
Jacob Magar <jmagar@gmail.com>
2026-06-25 02:38:36 -0400

    chore: bump android qa release versions

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.axon.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.4.1"
+        versionCode = 11
+        versionName = "1.4.2"
         manifestPlaceholders["appAuthRedirectScheme"] = "com.axon.app"
     }
 
